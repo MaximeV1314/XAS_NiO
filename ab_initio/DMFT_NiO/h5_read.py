b8d2ba2c1f2d5bfaae860c560e46b25e32d9420c
Maxime Vinteler <maxime.vinteler@yahoo.fr>
2026-03-13 18:09:35 +0100

    dmft?

---
+++ ab_initio/DMFT_NiO/h5_read.py
@@ -0,0 +1,8 @@
+from h5 import HDFArchive
+import numpy as np
+
+with HDFArchive("w90_to_triqs.h5", "r") as A:
+    hopping = A["dft_input"]["hopping"]
+
+print(hopping.shape)
+print(np.diag(np.round(np.sum(hopping[:,0,:,:], axis=0)/512, 4)))
